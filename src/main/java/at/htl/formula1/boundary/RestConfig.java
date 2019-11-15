0786799d01063c9b97aa5f2e3363a4c3d9e2044c
sonjacao <csonja1703@gmail.com>
2019-11-15 14:20:50 +0100

    Created RestConfig file

---
+++ src/main/java/at/htl/formula1/boundary/RestConfig.java
@@ -0,0 +1,8 @@
+package at.htl.formula1.boundary;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestConfig extends Application {
+}
