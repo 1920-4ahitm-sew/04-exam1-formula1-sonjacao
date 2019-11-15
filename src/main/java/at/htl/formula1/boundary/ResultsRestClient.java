ec97ea66147b8117f4c26d1f7268c99a1d6f5493
sonjacao <csonja1703@gmail.com>
2019-11-15 14:15:09 +0100

    Added code for persisting data into db table F1_RESULT from a REST server

--- src/main/java/at/htl/formula1/boundary/ResultsRestClient.java
+++ src/main/java/at/htl/formula1/boundary/ResultsRestClient.java
@@ -19,6 +19,9 @@ import javax.ws.rs.core.Response;
 public class ResultsRestClient {
 
 
+    @PersistenceContext
+    EntityManager em;
+
     public static final String RESULTS_ENDPOINT = "http://vm90.htl-leonding.ac.at/results";
     private Client client;
     private WebTarget target;
@@ -28,8 +31,11 @@ public class ResultsRestClient {
      * Dieses JsonArray wird an die Methode persistResult(...) übergeben
      */
     public void readResultsFromEndpoint() {
+        client = ClientBuilder.newClient();
+        target = client.target(RESULTS_ENDPOINT);
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
 
-        JsonArray payload = null;
+        JsonArray payload = response.readEntity(JsonArray.class);
 
         persistResult(payload);
     }
@@ -38,24 +44,76 @@ public class ResultsRestClient {
      * Das JsonArray wird durchlaufen (iteriert). Man erhäjt dabei Objekte vom
      * Typ JsonValue. diese werden mit der Methode .asJsonObject() in ein
      * JsonObject umgewandelt.
-     *
+     * <p>
      * zB:
      * for (JsonValue jsonValue : resultsJson) {
-     *             JsonObject resultJson = jsonValue.asJsonObject();
-     *             ...
-     *
-     *  Mit den entsprechenden get-Methoden können nun die einzelnen Werte
-     *  (raceNo, position und driverFullName) ausgelesen werden.
-     *
-     *  Mit dem driverFullName wird der entsprechende Driver aus der Datenbank ausgelesen.
-     *
-     *  Dieser Driver wird dann dem neu erstellten Result-Objekt übergeben
+     * JsonObject resultJson = jsonValue.asJsonObject();
+     * ...
+     * <p>
+     * Mit den entsprechenden get-Methoden können nun die einzelnen Werte
+     * (raceNo, position und driverFullName) ausgelesen werden.
+     * <p>
+     * Mit dem driverFullName wird der entsprechende Driver aus der Datenbank ausgelesen.
+     * <p>
+     * Dieser Driver wird dann dem neu erstellten Result-Objekt übergeben
      *
      * @param resultsJson
      */
     @Transactional
     void persistResult(JsonArray resultsJson) {
+        for (JsonValue jsonValue : resultsJson) {
+            JsonObject resultJson = jsonValue.asJsonObject();
 
+
+            Driver driver = em
+                    .createNamedQuery("Driver.findByName", Driver.class)
+                    .setParameter("NAME", resultJson.getString("driverFullName"))
+                    .getSingleResult();
+            Race race = em.find(Race.class, (long) resultJson.getInt("raceNo"));
+
+            Result result = new Result();
+            result.setDriver(driver);
+            result.setRace(race);
+            result.setPosition(resultJson.getInt("position"));
+
+            switch (result.getPosition()) {
+                case 1:
+                    result.setPoints(25);
+                    break;
+                case 2:
+                    result.setPoints(18);
+                    break;
+                case 3:
+                    result.setPoints(15);
+                    break;
+                case 4:
+                    result.setPoints(12);
+                    break;
+                case 5:
+                    result.setPoints(10);
+                    break;
+                case 6:
+                    result.setPoints(8);
+                    break;
+                case 7:
+                    result.setPoints(6);
+                    break;
+                case 8:
+                    result.setPoints(4);
+                    break;
+                case 9:
+                    result.setPoints(2);
+                    break;
+                case 10:
+                    result.setPoints(1);
+                    break;
+                default:
+                    result.setPoints(0);
+            }
+
+            em.persist(result);
+
+        }
     }
 
 }
