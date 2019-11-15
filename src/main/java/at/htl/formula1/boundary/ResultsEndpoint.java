880f2aebf7df6a22a55fdf1940100c25011bcfd0
sonjacao <csonja1703@gmail.com>
2019-11-15 14:49:19 +0100

    Added code to method getPointsSumOfDriver()

--- src/main/java/at/htl/formula1/boundary/ResultsEndpoint.java
+++ src/main/java/at/htl/formula1/boundary/ResultsEndpoint.java
@@ -4,6 +4,8 @@ import at.htl.formula1.entity.Driver;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonValue;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.*;
@@ -11,18 +13,32 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-
+@Path("results")
 public class ResultsEndpoint {
 
+    @PersistenceContext
+    EntityManager em;
 
     /**
      * @param name als QueryParam einzulesen
      * @return JsonObject
      */
-    public JsonObject getPointsSumOfDriver(
-            String name
-    ) {
-        return null;
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getPointsSumOfDriver(@QueryParam("name") String name) {
+        Driver driver = em
+                .createNamedQuery("Driver.findByName", Driver.class)
+                .setParameter("NAME", name)
+                .getSingleResult();
+        Long driverPoints = em
+                .createNamedQuery("Result.getPointsSumOfDriver", Long.class)
+                .setParameter("DRIVER", driver)
+                .getSingleResult();
+
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        builder.add("driver", driver.getName());
+        builder.add("points", driverPoints);
+        return builder.build();
     }
 
     /**
